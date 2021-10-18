86375ba20f1af988411b513a1a96c72c6fb32934
Tronikel <donatas@tronikel.com>
2021-10-18 19:04:11 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
                         items: [
                             {
                                 label: "Tutorial",
-                                to: "/docs/intro",
+                                to: "/",
                             },
                         ],
                     },
