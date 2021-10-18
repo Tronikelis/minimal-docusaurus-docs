84cfd8dc66459e9a3f57f2f455cf786d3ee995b1
Donatas Bucinskas <donatas@tronikel.com>
2021-10-18 19:00:21 +0300

    initial

---
+++ docusaurus.config.js
@@ -0,0 +1,71 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: "My Site",
+    tagline: "Dinosaurs are cool",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
+
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    routeBasePath: "/",
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "Tronikel",
+                logo: {
+                    alt: "My Site Logo",
+                    src: "img/logo.svg",
+                    href: "https://tronikel.com",
+                },
+                items: [],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Docs",
+                        items: [
+                            {
+                                label: "Tutorial",
+                                to: "/docs/intro",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Tronikel, Inc. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
